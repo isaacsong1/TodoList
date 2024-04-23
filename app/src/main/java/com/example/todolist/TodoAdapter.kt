8c579c4eaf5fb71a78cfad53a1113739a0af7c56
isaacsong1 <isaacsongs10@gmail.com>
2024-04-23 13:45:59 -0700

    use ctrl + i to add functions to be implemented

--- app/src/main/java/com/example/todolist/TodoAdapter.kt
+++ app/src/main/java/com/example/todolist/TodoAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.todolist
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class TodoAdapter (
@@ -8,5 +9,15 @@ class TodoAdapter (
 ) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
     class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) // TodoViewHolder class inherits RecyclerView with constructor itemView
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        TODO("Not yet implemented")
+    }
 
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
 }
