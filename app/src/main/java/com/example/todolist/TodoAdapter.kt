a410a4688d8bcf8c7b6faa45f5778ed1796bae06
isaacsong1 <isaacsongs10@gmail.com>
2024-04-23 13:45:35 -0700

    inherit from RecyclerView Adapter to TodoAdapter class

--- app/src/main/java/com/example/todolist/TodoAdapter.kt
+++ app/src/main/java/com/example/todolist/TodoAdapter.kt
@@ -5,6 +5,8 @@ import androidx.recyclerview.widget.RecyclerView
 
 class TodoAdapter (
     private val todos: MutableList<Todo>
-) {
+) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
     class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) // TodoViewHolder class inherits RecyclerView with constructor itemView
+
+
 }
