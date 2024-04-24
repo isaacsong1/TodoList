127ccaab038936d6026e67c99397fbb6cb91854c
isaacsong1 <isaacsongs10@gmail.com>
2024-04-24 14:39:34 -0700

    implement onCreateViewHolder func

--- app/src/main/java/com/example/todolist/TodoAdapter.kt
+++ app/src/main/java/com/example/todolist/TodoAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.todolist
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -10,14 +11,20 @@ class TodoAdapter (
     class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) // TodoViewHolder class inherits RecyclerView with constructor itemView
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
-        TODO("Not yet implemented")
+        return TodoViewHolder (
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_todo,
+                parent,
+                false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
-        TODO("Not yet implemented")
+
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+
     }
 }
